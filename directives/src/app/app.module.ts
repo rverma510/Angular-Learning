05d514be7df0c044925cb4709505dd619561b766
Raghav Verma <raghavverma4091@gmail.com>
2020-02-10 13:02:11 +0530

    Added ngIf component in directives

--- directives/src/app/app.module.ts
+++ directives/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IfComponent } from './components/if/if.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IfComponent
   ],
   imports: [
     BrowserModule
