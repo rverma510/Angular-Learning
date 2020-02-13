c92dcbd3a2e4e57306e2f811f5f9e7a614346728
Raghav Verma <raghavverma4091@gmail.com>
2020-02-13 11:10:08 +0530

    Modified uppercase-converter.service.ts to inject RESTful post service

--- postService/src/app/services/uppercase-converter.service.ts
+++ postService/src/app/services/uppercase-converter.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; 
 
 @Injectable({
   providedIn: 'root'
 })
 export class UppercaseConverterService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  
+  public convertToUppercase(obj): any{
+    return this.http.post('http://test-routes.herokuapp.com/test/uppercase', obj);
+  }
 }
