9753859d514d055829361e516d7ba3e0bbe66127
Raghav Verma <42772063+WolverineXR@users.noreply.github.com>
2020-03-29 16:22:36 +0530

    Modified controller to add get, post, put, delete functions

--- productAPI/controllers/product.controller.js
+++ productAPI/controllers/product.controller.js
@@ -19,4 +19,60 @@ router.get("/", (req, res)=>{
     })
 }); 
 
+router.get("/:id", (req, res)=>{
+    connection.query("select * from product where id="+req.params.id, (err, records, fields)=>{
+        if(err){
+            console.error("Error while fetching data");
+        }
+        else{
+            res.send(records);
+        }
+    })
+}); 
+
+router.post("/", (req, res)=>{
+    var id = req.body.id;
+    var name = req.body.name;
+    var description = req.body.description;
+    var price = req.body.price;
+    console.log("insert into product values("+id+", '"+name+"', '"+description+"', "+price+")");
+    connection.query("insert into product values("+id+", '"+name+"', '"+description+"', "+price+")",
+     (err, result)=>{
+        if(err){
+            console.error("Error while inserting data : "+ err);
+        }
+        else{
+            res.send({insert:"success"});
+        }
+    })
+});
+
+router.put("/", (req, res)=>{
+    var id = req.body.id;
+    var name = req.body.name;
+    var price = req.body.price;
+    console.log("update product set name='"+name+"',price="+price+" where id="+id+";");
+    connection.query("update product set name='"+name+"',price="+price+" where id="+id+";",
+     (err, result)=>{
+        if(err){
+            console.error("Error while inserting data : "+ err);
+        }
+        else{
+            res.send({update:"success"});
+        }
+    })
+}); 
+
+router.delete("/:id", (req, res)=>{
+    console.log("delete from product where id="+req.params.id+";");
+    connection.query("delete from product where id="+req.params.id+";", (err, records, fields)=>{
+        if(err){
+            console.error("Error while deleting data");
+        }
+        else{
+            res.send({delete:"success"});
+        }
+    })
+}); 
+
 module.exports = router;
--- productAPI/server.js
+++ productAPI/server.js
@@ -1,6 +1,9 @@
 var express = require("express");
+var bodyparser = require("body-parser");
 
 var app = express();
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:false}));
 
 var productAPI = require("./controllers/product.controller");
 app.use("/api/products", productAPI);
